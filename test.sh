a2d7e6d833f2285f994a2ef5a69eb598371c550b
Hendrik Ulbrich <hendrikulbrich@outlook.de>
2019-03-07 23:20:27 +0100

    Improve bash script to adjust output file via parameter

--- test.sh
+++ test.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
+if [ $# -le 1 ] 
+then 
+    echo "Usage: ./test.sh 50 test.log"
+    exit 1
+fi 
+
 xcodebuild install -alltargets
 
 counter=1
 while [ $counter -le $1 ]
 do
     echo $counter
-    ios-deploy -d -m -W -I -b /tmp/argue-with-ar-ios.dst/Applications/argue-with-ar-ios.app | grep XXXXXXX* >> test.log
+    ios-deploy -d -m -W -I -b /tmp/argue-with-ar-ios.dst/Applications/argue-with-ar-ios.app | grep XXXXXXX* >> $2
     ((counter++))
 done
 
 # Converting greppy output to CSV format
-tr -d '\n\r' < test.log > temp.log && mv temp.log test.log
-sed -e "s/XXXXXXX: /,/g" -e "s/,//" test.log > temp.log && mv temp.log test.log
+tr -d '\n\r' < $2 > temp.log && mv temp.log $2
+sed -e "s/XXXXXXX: /,/g" -e "s/,//" $2 > temp.log && mv temp.log $2
 
 while true; do
     say "ICH BIN FERTIG. GIB MIR WAS NEUES ZU TUN"
